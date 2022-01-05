fbc0648dd84aa831229c28e5abcd337c2a1315cf
nguyên <72590935+doannhatnguyen97@users.noreply.github.com>
2022-01-05 17:42:38 +0700

    update payment method page

--- js/fn-global.js
+++ js/fn-global.js
@@ -1,8 +1,28 @@
 var jDocument = $(document), widthWindow = $(window).width(), responsive = widthWindow > 992;
 
 jDocument.ready(function () {
+    // Init checked radio button
+    $('.check-form').checkList();
 
 });
+
+jQuery.fn.extend({
+    checkList: function () {
+        return this.each(function (e) {
+            var option = $(this).find('.radio');
+            option.on('click', function () {
+                option.removeClass('checked');
+                $(this).addClass('checked');
+            });
+        });
+    },
+});
+
+
+
+
+
+
 
 vnSlide = () => {
     new Swiper(".vnSlide1", {
